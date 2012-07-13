c207e3bb76d063e435dc7cbafebccaeef583f53c
olivier lamy <olamy@apache.org>
2012-07-13 10:23:06 +0200

    missed to add a file

---
+++ src/main/java/org/jenkinsci/plugins/maveninvoker/results/MavenInvokerResult.java
@@ -0,0 +1,67 @@
+package org.jenkinsci.plugins.maveninvoker.results;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.Serializable;
+
+/**
+ * @author Olivier Lamy
+ */
+public class MavenInvokerResult
+    implements Serializable
+{
+    public String project;
+
+    public String name;
+
+    public String description;
+
+    public String result;
+
+    public String failureMessage;
+
+    public double time;
+
+    public String type;
+
+    public String buildLog;
+
+
+    public MavenInvokerResult()
+    {
+        // no op
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder();
+        sb.append( "MavenInvokerResult" );
+        sb.append( "{project='" ).append( project ).append( '\'' );
+        sb.append( ", name='" ).append( name ).append( '\'' );
+        sb.append( ", description='" ).append( description ).append( '\'' );
+        sb.append( ", result='" ).append( result ).append( '\'' );
+        sb.append( ", failureMessage='" ).append( failureMessage ).append( '\'' );
+        sb.append( ", time=" ).append( time );
+        sb.append( ", type='" ).append( type ).append( '\'' );
+        sb.append( ", buildLog='" ).append( buildLog ).append( '\'' );
+        sb.append( '}' );
+        return sb.toString();
+    }
+}
